62a0578ab5aba63689168b9b54ad50620994d71a
Julian Viereck <julian.viereck@gmail.com>
2015-03-28 12:11:23 +0100

    Init commit: Not working site in yet ;)

---
+++ mozilla_org/mozilla_org_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "ZURICH",
+      'continent': "EU",
+      'country': "CH",
+      'region': ""
+    },
+    'ip':"80.219.169.28"
+  }]);
+})
+//
+()
+
+;
